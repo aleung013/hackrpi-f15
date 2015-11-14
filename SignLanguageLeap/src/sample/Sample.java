8af2baa5646a0d80f63fb9cd7f0459f934428a9e
RatedRion <ratedrion@gmail.com>
2015-11-14 15:41:51 -0500

    Test commit

--- SignLanguageLeap/src/sample/Sample.java
+++ SignLanguageLeap/src/sample/Sample.java
@@ -5,11 +5,11 @@ import java.util.*;
 
 public class Sample {
 	public static void main(String[] args){
-		//Create a sample listener and controller
+		//Create a sample listener and controller.
 		SampleListener listener = new SampleListener();
 		Controller controller = new Controller();
 		
-		//Have the samplelistener receive events from the controller
+		//Have the samplelistener receive events from the controller.
 		controller.addListener(listener);
 		
 		System.out.println("Press Enter to quit...");
